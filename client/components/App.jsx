8e8e957610a2f7d5c5178156ab3f1f6500362c6f
Oussama-Fajraoui <oussama.fajraoui1@gmail.com>
2022-06-24 11:07:10 +0100

    Fixing components

---
+++ client/components/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./Header";
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+    render() {
+        return (
+            <div>
+                <Header />
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ client/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+ class Header extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+    render() {
+    return (
+        <div>
+        <h1>Movie App</h1>
+        </div>
+    );
+    }
+ }
+ export default Header;
