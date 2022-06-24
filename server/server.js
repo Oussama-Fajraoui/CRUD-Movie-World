52011d7792a25d382b06b1cb607ebd480a35b982
Oussama-Fajraoui <oussama.fajraoui1@gmail.com>
2022-06-24 12:50:44 +0100

    working on server file

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const movieSrc = require("../server/src") 
+
+app.use(express.json());
+
+app.use(express.static("./public"));
+
+app.use("api/movie",movieSrc);
+
+module.exports = app
